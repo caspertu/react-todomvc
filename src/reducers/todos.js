4b73a5269fe7b72f8a39d4eb5478be99781af1bc
caspertu <caspertu@gmail.com>
2019-07-23 11:19:31 +0800

    LocalStorage

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -7,18 +7,33 @@ import {
   CLEAR_COMPLETED,
 } from '../constants/ActionTypes'
 
-const initialState = [
-  {
-    text: 'Use Redux',
-    completed: false,
-    id: 0
-  }
-]
+// const mockInitialState = [
+//   {
+//     text: 'Use Redux',
+//     completed: false,
+//     id: 0
+//   }
+// ]
+
+// localStorage.setItem('todos', JSON.stringify(mockInitialState))
+
+const getTodos = () => {
+  return JSON.parse(localStorage.getItem('todos'))
+}
+
+const saveTodos = (todos) => {
+  localStorage.setItem('todos', JSON.stringify(todos))
+  return todos
+}
+
+const initialState = getTodos()
+
+console.log(initialState);
 
 export default function todos(state = initialState, action) {
   switch (action.type) {
     case ADD_TODO:
-      return [
+      const todos = [
         ...state,
         {
           id: state.reduce((maxId, todo) => Math.max(todo.id, maxId), -1) + 1,
@@ -26,33 +41,34 @@ export default function todos(state = initialState, action) {
           text: action.text
         }
       ]
+      return saveTodos(todos)
 
     case DELETE_TODO:
-      return state.filter(todo => todo.id !== action.id)
+      return saveTodos(state.filter(todo => todo.id !== action.id))
 
     case EDIT_TODO:
-      return state.map(todo => 
+      return saveTodos(state.map(todo => 
         todo.id === action.id ? 
           ({ ...todo, text: action.text }) :
           todo
-      )
+      ))
 
     case COMPLETE_TODO:
-      return state.map(todo =>
+      return saveTodos(state.map(todo =>
         todo.id === action.id ?
           { ...todo, completed: !todo.completed } :
           todo
-      )
+      ))
 
     case COMPLETE_ALL_TODOS:
       const areAllMarked = state.every(todo => todo.completed)
-      return state.map(todo => ({
+      return saveTodos(state.map(todo => ({
         ...todo,
         completed: !areAllMarked
-      }))
+      })))
 
     case CLEAR_COMPLETED:
-      return state.filter(todo => todo.completed === false)
+      return saveTodos(state.filter(todo => todo.completed === false))
 
     default:
       return state
